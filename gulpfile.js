a7efa3a696ef999d034b9ed1f8cfd067b6d5d1fc
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-28 15:41:34 +0200

    add libs script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const concat = require('gulp-concat');
 global.$ = {
   path: {
     task: require('./gulp/path/tasks.js')
@@ -12,7 +12,15 @@ global.$ = {
 $.path.task.forEach(function (taskPath) {
   require(taskPath)();
 });
+$.gulp.task('main-libs', function () {
+  return $.gulp.src([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/slick-carousel/slick/slick.min.js'
+  ])
+      .pipe(concat('main-libs.min.js'))
+      .pipe($.gulp.dest('./dist/js/'))
 
+});
 $.gulp.task('dev', $.gulp.series(
     'clean',
     $.gulp.parallel(
@@ -24,13 +32,5 @@ $.gulp.task('dev', $.gulp.series(
         'svg'
     )
 ));
-
-// $.gulp.task('default', $.gulp.series(
-//     'dev',
-//     $.gulp.parallel(
-//         'watch',
-//         'serve-new'
-//     )
-// ));
 
 $.gulp.task('default', $.gulp.series('dev', 'watch', 'serve-new'));
---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('.single-item').slick({
+    dots: false,
+    arrow: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
+});
