5bd4f97c49ea0d0b00a0562bf5f5bb2c5fd3e81f
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-29 14:39:14 +0200

    add jquery-ui script/style

--- src/js/main.js
+++ src/js/main.js
@@ -59,4 +59,16 @@ $(document).ready(function () {
         }
     }
   });
+
+  /* range slider */
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [46, 1522],
+    slide: function (event, ui) {
+      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+    }
+  });
+  $("#amount").val("$" + $("#slider-range").slider("values", 0) + " - $" + $("#slider-range").slider("values", 1));
 });
