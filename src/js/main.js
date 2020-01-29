89e3c9524492cda41bfa685d433b7f66110e7a4a
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-29 12:22:01 +0200

    footer, new products and news add style

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.single-item').slick({
     dots: false,
     arrow: true,
@@ -22,5 +22,41 @@ $(document).ready(function() {
         slidesToShow: 1
       }
     }]
+  });
+
+  /* show all text on seo */
+  $(".offer-description__link").click(function () {
+    var t = this;
+    if ($(this).data("temp") || $(this).data("temp", $(this).text()),
+    "" != $(this).data("link")) {
+      var e = $('[data-id="' + $(this).data("link") + '"]');
+      if (0 != e.length)
+        if ($(this).hasClass("close"))
+          $(this).css("opacity", 0),
+              e.removeClass("open"),
+              e.stop().animate({
+                "max-height": 81
+              }, 500, "linear", function () {
+                $(t).stop().animate({
+                  opacity: 1
+                }, 500),
+                    $(t).removeClass("close");
+              }),
+              $(this).text($(this).data("temp"));
+        else {
+          $(this).css("opacity", 0),
+              e.addClass("open");
+          var n = e.children().eq(0).outerHeight();
+          e.stop().animate({
+            "max-height": n
+          }, 500, "linear", function () {
+            $(t).stop().animate({
+              opacity: 1
+            }, 500),
+                $(t).addClass("close");
+          }),
+              ($(this).text() === "Скрыть") ? $(this).text("Показать больше...") : $(this).text("Скрыть");
+        }
+    }
   });
 });
